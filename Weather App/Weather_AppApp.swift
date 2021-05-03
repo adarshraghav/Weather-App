cc36243190bbb3582e6183e9743e259d863699cd
Adarsh Raghav <aadarshraghav@gmail.com>
2021-05-03 10:36:31 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Adarsh Raghav on 03/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
